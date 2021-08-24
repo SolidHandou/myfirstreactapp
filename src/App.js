de77db90673d3011b25a1ef9e6365a4c0b103181
SolidHandou <chartgonz@gmail.com>
2021-08-24 15:56:42 -0400

    Init react app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Alguien ayudame con <strong>Candice</strong>
+        </p>
+        <p>
+         Thingy
         </p>
         <a
           className="App-link"
